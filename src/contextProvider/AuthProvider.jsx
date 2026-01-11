819c528f436686097580fe49acb9f9d5e7fadf0f
aunirudrad <anu15-4170@diu.edu.bd>
2026-01-12 02:38:30 +0600

    auth app added

---
+++ src/contextProvider/AuthProvider.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AuthProvider = new GoogleAuthProvider();
+
+
+const AuthProvider = ({children}) => {
+    
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AuthProvider;
